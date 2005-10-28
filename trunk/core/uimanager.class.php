1772e0859b08e749f6265be4f180f17fdbc41e52
nathansamson <nathansamson@c84c3d3a-7f03-0410-8fc1-bc7ae66d8409>
2005-10-28 21:40:03 +0000

    First skinnable output is created whohoo

--- trunk/core/uimanager.class.php
+++ trunk/core/uimanager.class.php
@@ -23,11 +23,15 @@
 include_once ('core/database.class.php');
 include_once ('core/user.class.php');
 include_once ('core/config.class.php');
+define ('MORGOS_VERSION','0.1');
 /** \class UIManager
  * class that take care of the main UI layer, extensionhandling and HTML output.
  *
  * \author Nathan Samson
  * \version 0.1svn
+ * \bug not compatible with PHP lower than 4.1  (use of version_compare)
+ * \bug not compatible with PHP 4.0.4 and lower (use of array_search)
+ * \bug not compatible with PHP 4.0.0 and lower (use of trigger_error)
 */
 class UIManager {
 	/*private $DBManager;
@@ -45,6 +49,9 @@ class UIManager {
 		$this->DBManager = new genericDatabase ();
 		$this->genDB = $this->DBManager->load ($this->config->getConfigItem ('/database/type',TYPE_STRING));
 		$this->user = new user ($this->genDB);
+		
+		$this->loadSkin ('MorgOS Default');
+		$this->loadPage ('index.html');
 	}
 	
 	/** \fn getGenericDB ()
@@ -64,6 +71,49 @@ class UIManager {
 	/*public*/ function &getConfigClass () {
 		return $this->config;
 	}
+	
+	/** \fn loadPage ($pageName,$authorized = false,$authorizedAsAdmin = false) 
+	 * Echo a page with name $pageName. If the user needs to be logged in to view this page (and he isn't) 
+	 * an error is triggered, if he needs to be admin and he isn't als an error is triggered
+	 *
+	 * \todo trigger errors if user is not logged in, but userclass needs to be written first
+	*/ 
+	/*public*/ function loadPage ($pageName,$authorized = false,$authorizedAsAdmin = false) {
+		// User code needs to be implemented first
+		readfile ($this->skinPath . '/' . $pageName);
+	}
+	 
+	/** \fn loadSkin ($skinName)
+	 * Loads all skin options
+	 *
+	 * \param $skinName (string) the name of the skin to load
+	*/
+	/*private*/ function loadSkin ($skinName) {
+		$supported = array ();
+		$handler = opendir ('skins');
+		// $files = scandir ('skins'); PHP5 only :( 
+		// foreach ($files as $file) PHP5 only :(
+		while (false !== ($file = readdir ($handler))) {
+			$supSkins = array ();
+			$skinPaths = array ();
+			// do not start with a point (.)
+			if ((preg_match ('/^\w.*/i', $file) == 1) and (is_dir ('skins/' . $file)) and (is_file ('skins/' . $file . '/skin.ini'))) {
+				unset ($skin);
+				$skin = parse_ini_file ('skins/' . $file . '/skin.ini',true);
+				if (version_compare ($skin['general']['minversion'],MORGOS_VERSION,'<=') and (version_compare ($skin['general']['maxversion'],MORGOS_VERSION,'>='))) {
+					$supSkins[] = $skin['general']['name'];
+					$skinPaths[] = 'skins/' . $file . '/';
+				}
+			}
+		}
+		$key = array_search ($skinName,$supSkins);
+		// needs to be 3 = (===) because the key can be 0 and than it is the same as false
+		// to be compatible with PHP <= 4.2 we need to have === NULL
+		if (($key === false) or ($key === NULL)) {
+			trigger_error ('Couldn\'t load skin, unsupported skin',E_USER_ERROR);
+		} else {
+			$this->skinPath = $skinPaths[$key];
+		}
+	}
 }
-
 ?>
