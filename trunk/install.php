08f1364841a834c7797fe11ea1bb898284247e19
nathansamson <nathansamson@c84c3d3a-7f03-0410-8fc1-bc7ae66d8409>
2005-10-29 20:27:35 +0000

    Adds the skins, adds basic installation framework (but much work is required on this)

---
+++ trunk/install.php
@@ -0,0 +1,43 @@
+<?php
+/* MorgOS is a Content Management System written in PHP
+ * Copyright (C) 2005 MorgOS
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
+*/
+if (array_key_exists ('phase', $_GET) == false) {
+	$phase = 'start';
+} else {
+	$phase = $_GET['phase'];
+}
+switch ($phase) {
+	case 'start':
+		readfile ('install/license.html');
+		break;
+	case 'check':
+		readfile ('install/check.html');
+		break;
+	case 'config':
+		readfile ('install/config.html');
+		break;
+	case 'checkdbconn':
+		readfile ('install/checkdbconn.html');
+		break;
+	case 'install':
+		readfile ('install/installing.html');
+		break;
+	case 'done':
+		readfile ('install/done.html');
+		break;
+}
+?>
