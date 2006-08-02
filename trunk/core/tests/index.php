5548dc114c3d6153ea56d6f0773a057d3d9bf34b
nathansamson <nathansamson@c84c3d3a-7f03-0410-8fc1-bc7ae66d8409>
2006-08-02 13:06:46 +0000

    Delete everything

--- trunk/core/tests/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once ('test.class.php');
-	initTester (true, 'MorgOS');
-	include_once ('compatible.test.php');
-	include_once ('config.test.php');
-	runTests ();
-	showAllResults ();
-	deInitTester ();
-?>
