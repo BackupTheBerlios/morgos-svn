0c31045425cf549a3aeaf8e72e41426aaa8057ad
nathansamson <nathansamson@c84c3d3a-7f03-0410-8fc1-bc7ae66d8409>
2006-08-03 18:12:35 +0000

    Adding stub news/user classes

---
+++ trunk/core/newsmanager.class.php
@@ -0,0 +1,77 @@
+<?php
+/* MorgOS is a Content Management System written in PHP
+ * Copyright (C) 2005-2006 MorgOS
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
+*/
+/** \file newsmanager.class.php
+ * File that take care of the newsmanager
+ *
+ * @since 0.2
+ * @author Nathan Samson
+ * @license GPL
+*/
+
+include_once ('core/newscommentitem.class.php');
+include_once ('core/newstopic.class.php');
+include_once ('core/newsitem.class.php');
+
+class newsmanager {
+
+	function newsmanager () {
+	}
+
+	/*Public newsitem functions*/
+	
+	function getAllNewsItems () {
+	}
+	
+	function postNewsItem () {
+	}
+	
+	function addOptionsToNewsItem () {
+	}
+	
+	function areNewsItemsGloballyDisabled () {
+	}	
+	
+	function setNewsItemsGloballyDisabled () {
+	}	
+	
+	/*Public topics functions*/
+	
+	function getAllTopics () {
+	}
+	
+	function addTopicToDatabase () {
+	}
+	
+	function removeTopicFromDatabase () {
+	}
+	
+	function addOptionsToTopic () {
+	}
+	
+	/*Public commentitem functions*/
+	
+	function addOptionToComments () {
+	}
+	
+	function areCommentsGloballyDisabled () {
+	}
+	
+	function setCommentGloballyDisabled () {
+	}
+	
+}
---
+++ trunk/core/user.class.php
@@ -0,0 +1,74 @@
+<?php
+/* MorgOS is a Content Management System written in PHP
+ * Copyright (C) 2005-2006 MorgOS
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
+*/
+/** \file user.class.php
+ * File that take care of one user
+ *
+ * @since 0.2
+ * @author Nathan Samson
+ * @license GPL
+*/
+
+class user {
+
+	function user () {
+	}
+	
+	/*Public initters*/
+	
+	function initFromDatabaseID () {
+	}
+	
+	function initFromDatabaseLogin () {
+	}
+	
+	function initFromDatabaseEmail () {
+	}
+	
+	function initFromArray () {
+	}
+	
+	/*Public functions*/
+	
+	function addToDatabase () {
+	}
+	
+	function isInDatabase () {
+	}
+	
+	function updateToDatabase () {
+	}
+	
+	function updateFromArray () {
+	}
+	
+	function addToGroup () {
+	}
+	
+	function hasRight () {
+	}
+	
+	function getLogin () {}
+	function getEmail () {}
+	function getID () {}
+	function getAllGroups () {}
+	function getOption () {}
+	
+	/*Private initters*/
+	function initEmpty () {
+	}
+}
