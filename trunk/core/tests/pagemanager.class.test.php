fa86471a806350cd96665f1201cdcdfa307f764d
nathansamson <nathansamson@c84c3d3a-7f03-0410-8fc1-bc7ae66d8409>
2006-08-24 23:25:04 +0000

    Fixes in databaseObject (need more fixes) Added first bits for making translation of pages possible Adding code for adding/removing pages from the database

--- trunk/core/tests/pagemanager.class.test.php
+++ trunk/core/tests/pagemanager.class.test.php
@@ -29,6 +29,17 @@ class pageManagerTest extends TestCase {
 		$this->pageManager = new pageManager ($dbModule);
 	}
 	
+	function testPageExists () {
+		$this->assertTrue ($this->pageManager->pageExists ('Home'), 'Expected true');
+		$this->assertFalse ($this->pageManager->pageExists ('NotExistingPage'), 'Expected false');
+	}
+	
+	function testPageInitFromGenericName () {
+		$page = $this->pageManager->newPage ();
+		$r = $page->initFromGenericName ('notExistingPage');
+		$this->assertEquals ($r, "ERROR_PAGE_GENERICNAME_DOESNT_EXISTS notExistingPage");
+	}	
+	
 	function testGetMenu () {
 		$root = $this->pageManager->newPage ();
 		$root->initFromGenericName ('site');
@@ -59,9 +70,101 @@ class pageManagerTest extends TestCase {
 		$this->assertEquals (array (), $this->pageManager->getAllOptionsForPage (), 'Wrong options 3');
 		$r = $this->pageManager->removeOptionForPage ('optionName');
 		$this->assertEquals ("ERROR_PAGEMANAGER_OPTION_FORPAGE_DOESNT_EXISTS optionName", $r, 'Wrong error 2');
-		
-				
 	}
+	
+	function testAddPageToDatabase () {
+		$root = $this->pageManager->newPage ();
+		$root->initFromGenericName ('site');	
+	
+		$development = $this->pageManager->newPage ();
+		$array = array ();
+		$array['genericName'] = 'Development';
+		$array['genericContent'] = 'This is the development page.';
+		$array['parentPageID'] = $root->getID ();
+		$array['placeInMenu'] = 3; //before packages, after news
+		$development->initFromArray ($array);
+		$r = $this->pageManager->addPageToDatabase ($development);
+		$this->assertFalse (isError ($r), 'Unexpected error');		
+		
+		$home = $this->pageManager->newPage ();
+		$home->initFromGenericName ('Home');
+		$news = $this->pageManager->newPage ();
+		$news->initFromGenericName ('News');
+		$packages = $this->pageManager->newPage ();
+		$packages->initFromGenericName ('Packages');	
+		
+		$siteMenu = $this->pageManager->getMenu ($root);	
+		$this->assertEquals (array ($home, $news, $development, $packages), $siteMenu, 'Wronge menu order');
+		
+		$appendPage = $this->pageManager->newPage ();
+		$array = array ();
+		$array['genericName'] = 'lastPage';
+		$array['genericContent'] = 'This is the last page in the menu.';
+		$array['parentPageID'] = $root->getID ();
+		$array['placeInMenu'] = 0;
+		$a = $appendPage->initFromArray ($array);
+		$r = $this->pageManager->addPageToDatabase ($appendPage);
+		$this->assertFalse (isError ($r), 'Unexpected error');
+		$siteMenu = $this->pageManager->getMenu ($root);	
+		$this->assertEquals ('5', $appendPage->getPlaceInMenu ());
+		$this->assertEquals (array ($home, $news, $development, $packages, $appendPage), $siteMenu, 'Wronge menu order');
+		
+		$r = $this->pageManager->addPageToDatabase ($appendPage);
+		$this->assertEquals ("ERROR_PAGEMANAGER_PAGE_EXISTS lastPage", $r, "Wrong error returned");
+	}
+	
+	function testRemovePageFromDatabase () {
+		$root = $this->pageManager->newPage ();
+		$root->initFromGenericName ('site');	
+
+		$development = $this->pageManager->newPage ();
+		$development->initFromGenericName ('Development');
+		
+		$r = $this->pageManager->removePageFromDatabase ($development);
+		$this->assertFalse (isError ($r), 'Unexpected error 1');
+		
+		
+		$siteMenu = $this->pageManager->getMenu ($root);
+		$home = $this->pageManager->newPage ();
+		$home->initFromGenericName ('Home');		
+		$news = $this->pageManager->newPage ();
+		$news->initFromGenericName ('News');
+		$packages = $this->pageManager->newPage ();
+		$packages->initFromGenericName ('Packages');
+		$lastPage = $this->pageManager->newPage ();	
+		$lastPage->initFromGenericName ('lastPage');		
+		
+		$this->assertEquals (array ($home, $news, $packages, $lastPage), $siteMenu, 'Wronge menu order');
+		$packages = $this->pageManager->newPage ();
+		$packages->initFromGenericName ('Packages');
+		$this->assertEquals ('3', $packages->getPlaceInMenu ());	
+					
+		$r = $this->pageManager->removePageFromDatabase ($lastPage);
+		$this->assertFalse (isError ($r), 'Unexpected error 2');
+		$siteMenu = $this->pageManager->getMenu ($root);
+		$this->assertEquals (array ($home, $news, $packages), $siteMenu, 'Wronge menu order');
+		
+		$r = $this->pageManager->removePageFromDatabase ($lastPage);
+		$this->assertEquals ("ERROR_PAGEMANAGER_PAGE_DOESNT_EXISTS lastPage", $r, 'Wrong error');
+		$siteMenu = $this->pageManager->getMenu ($root);
+		$this->assertEquals (array ($home, $news, $packages), $siteMenu, 'Wronge menu order');
+	}
+	
+	function testOptionsForTranslatedPage () {
+		$r = $this->pageManager->addOptionToTranslatedPage ('translator', 'int (11)');
+		$this->assertFalse (isError ($r), 'Unexpected error');
+		$this->assertEquals (array ('translator'=>null), $this->pageManager->getAllOptionsForTranslatedPage (), 'Wrong options 1');
+		$r = $this->pageManager->addOptionToTranslatedPage ('translator', 'varchar(20)');
+		$this->assertEquals ("ERROR_PAGEMANAGER_OPTION_FORTRANSLATEDPAGE_EXISTS translator", $r, 'Wrong error');
+		$this->assertEquals (array ('translator'=>null), $this->pageManager->getAllOptionsForTranslatedPage (), 'Wrong options 2');
+		
+		$r = $this->pageManager->removeOptionForTranslatedPage ('translator');
+		$this->assertFalse (isError ($r), 'Unexpected error 2');
+		$this->assertEquals (array (), $this->pageManager->getAllOptionsForTranslatedPage (), 'Wrong options 3');
+		$r = $this->pageManager->removeOptionForTranslatedPage ('translator');
+		$this->assertEquals ("ERROR_PAGEMANAGER_OPTION_FORTRANSLATEDPAGE_DOESNT_EXISTS translator", $r, 'Wrong error 2');		
+	}
+	
 }
 
 
