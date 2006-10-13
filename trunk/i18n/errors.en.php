5240212a97e5e488e3cb3243c34f04746f124d1c
nathansamson <nathansamson@c84c3d3a-7f03-0410-8fc1-bc7ae66d8409>
2006-10-13 19:42:03 +0000

    Removed unused file, added a release tool

--- trunk/i18n/errors.en.php
+++
@@ -1,5 +0,0 @@
-<?php
-$errorStrings['EMPTY_INPUT'] = 'Please, fill %1 in.';
-$errorStrings['PASSWORDS_NOT_EQUAL'] = 'Passwords didn\'t match.';
-$errorStrings['INVALID_CHOICE'] = 'You made an invalid choice for %2, you gave %1.';
-?>
